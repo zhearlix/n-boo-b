22ff582da591d0b1dae7e40c49840ab01f890664
zhearlixx <85249694+zhearlix@users.noreply.github.com>
2021-07-19 00:31:44 -0400

    Add files via upload

---
+++ download_images.py
@@ -0,0 +1,49 @@
+from pixabay import Image, Video
+import pprint
+import requests
+import shutil
+
+API_KEY = '22143093-6199c7e5e2b15b55d039ca367'
+image = Image(API_KEY)
+
+j=1
+for n in range(1,100):
+    ims = image.search(
+                q="seal",
+                lang="en",
+                image_type="all",
+                orientation="all",
+                category="animal",
+                min_width=0,
+                min_height=0,
+                colors="",
+                editors_choice="false",
+                safesearch="false",
+                order="popular",
+                page=n,
+                per_page=200,
+                callback="",
+                pretty="true"
+
+                )
+
+#hits=ims['total']
+#print(hits)    
+#print(ims)
+
+    #pp=pprint.PrettyPrinter(indent=4)
+    for i in range(0,200):
+        payload=ims['hits'][i]['largeImageURL']
+        resp = requests.get(payload, stream=True)
+        local_file = open(str(j)+"seal.jpg", 'wb')
+        resp.raw.decode_content = True
+        shutil.copyfileobj(resp.raw, local_file)
+        del resp
+
+
+
+        print(str(j)+"https://pixabay.com/es/images/search/seal/: {}".format(payload))
+        j=j+1 
+        #urllib.request.urlretrieve(payload,i)
+
+    #pp.pprint(ims)
